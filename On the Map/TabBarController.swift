f5f9345482cd395bec6a18ce1b9cd78fb395ed4d
Steven Chen <stevenchen@Stevens-MacBook-Pro.local>
2016-09-04 19:23:26 -0500

    Move addlocation and update location api code to udacityClient Add tabbarController

---
+++ On the Map/TabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarController.swift
+//  On the Map
+//
+//  Created by Steven Chen on 9/3/16.
+//  Copyright © 2016 Steven Chen. All rights reserved.
+//
+
+import Cocoa
+
+class TabBarController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
